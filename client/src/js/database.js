36e91cd5d1df1e3ab88101aaeec54350eccd5fe6
brake <riaconoo@icloud.com>
2022-09-22 15:07:18 +1000

    updated service workers, database creation, and manifest inject

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,40 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('executed putDB');
+
+    const jateDB = await openDB('jate', 1);
+
+    const tx = jateDB.transaction('jate', 'readwrite');
+
+    const store = tx.objectStore('jate');
+
+    const request = store.add({ content: content });
+
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
+} 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+    console.log('executed getDB');
+  
+    const jateDb = await openDB('jate', 1);
+
+    // Create a new transaction and specify the database and data privileges.
+    const tx = jateDb.transaction('jate', 'readonly');
+
+    // Open up the desired object store.
+    const store = tx.objectStore('jate');
+
+    // Use the .getAll() method to get all data in the database.
+    const request = store.getAll();
+
+    // Get confirmation of the request.
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+} 
 
 initdb();
